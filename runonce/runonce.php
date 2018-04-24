7917361fe8677cd3b672d3c0dff0ee6f03805adc
Sven Baumann <baumann.sv@gmail.com>
2018-04-24 20:50:36 +0200

    Add missing

--- runonce/runonce.php
+++ runonce/runonce.php
@@ -12,6 +12,7 @@
  *
  * @package    MetaModels
  * @subpackage AttributeGeoDistance
+ * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @copyright  2012-2018 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_geodistance/blob/master/LICENSE LGPL-3.0-or-later
