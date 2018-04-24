04ea48058d53e9693910d511af16aff16a09064b
Sven Baumann <baumann.sv@gmail.com>
2018-04-24 15:52:51 +0200

    Add the missing phpunit bootstrap

---
+++ tests/bootstrap.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of MetaModels/attribute_alias.
+ *
+ * (c) 2012-2018 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels
+ * @subpackage AttributeGeoDistance
+ * @author     Stefan Heimes <stefan_heimes@hotmail.com>
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2012-2018 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_geodistance/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+error_reporting(E_ALL);
+
+function includeIfExists($file)
+{
+	return file_exists($file) ? include $file : false;
+}
+
+if (
+	// Locally installed dependencies
+	(!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php'))
+	// We are within an composer install.
+	&& (!$loader = includeIfExists(__DIR__.'/../../../autoload.php'))) {
+	echo 'You must set up the project dependencies, run the following commands:'.PHP_EOL.
+		'curl -sS https://getcomposer.org/installer | php'.PHP_EOL.
+		'php composer.phar install'.PHP_EOL;
+	exit(1);
+}
