bd9cf10d29b16b3a420269c3d35d3752a420a066
Mini Model <minimodel@metamodel.me>
2018-04-25 10:14:41 +0200

    Updated translations from transifex.

---
+++ contao/languages/et/tl_metamodel_attribute.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * @link https://www.transifex.com/signup/
+ * @link https://www.transifex.com/projects/p/$$project$$/language/$$lang$$/
+ *
+ * last-updated: 2018-04-25T10:14:35+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['countrymode_options']['none']         = 'Pole';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['first_attr_id']['0']                  = 'Omadus - Laiuskraad';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['perimetersearch']['open_street_maps'] = 'OpenStreetMap';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['second_attr_id']['0']                 = 'Omadus - Pikkuskraad';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['single_attr_id']['0']                 = 'Omadus';
+
