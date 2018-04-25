0b858f273089256fdde8829315f8596870c96a07
Sven Baumann <baumann.sv@gmail.com>
2018-04-25 04:29:48 +0200

    Support the contao managed edition

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * This file is part of MetaModels/attribute_alias.
+ *
+ * (c) 2012-2018 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels
+ * @subpackage AttributeGeoDistanceBundle
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2012-2018 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_geodistance/blob/master/LICENSE LGPL-3.0-or-later
+ * @filesource
+ */
+
+namespace MetaModels\AttributeGeoDistanceBundle\ContaoManager;
+
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use MetaModels\AttributeGeoDistanceBundle\MetaModelsAttributeGeoDistanceBundle;
+use MetaModels\CoreBundle\MetaModelsCoreBundle;
+use MetaModels\FilterPerimetersearchBundle\MetaModelsFilterPerimetersearchBundle;
+
+/**
+ * Contao Manager plugin.
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(MetaModelsAttributeGeoDistanceBundle::class)
+                ->setLoadAfter(
+                    [
+                        MetaModelsCoreBundle::class,
+                        MetaModelsFilterPerimetersearchBundle::class
+                    ]
+                )
+                ->setReplace(['metamodelsattribute_geodistance'])
+        ];
+    }
+}
