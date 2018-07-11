dc2265a7140c82b4a0a4b499ed5c58683ce3fc41
Sven Baumann <baumann.sv@gmail.com>
2018-07-11 12:16:03 +0200

    Fix author validation issue

--- runonce/runonce.php
+++ runonce/runonce.php
@@ -12,7 +12,6 @@
  *
  * @package    MetaModels
  * @subpackage AttributeGeoDistance
- * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @copyright  2012-2018 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_geodistance/blob/master/LICENSE LGPL-3.0-or-later
