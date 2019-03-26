258211f343f8bde3befcd5ef5c2198ca40547ea1
Sven Baumann <baumann.sv@gmail.com>
2019-03-26 18:29:45 +0100

    Fix phpcs issue

--- src/Attribute/GeoDistance.php
+++ src/Attribute/GeoDistance.php
@@ -64,9 +64,10 @@ class GeoDistance extends BaseComplex
      * Note that you should not use this directly but use the factory classes to instantiate attributes.
      *
      * @param IMetaModel            $metaModel        The MetaModel instance this attribute belongs to.
-     * @param array                 $data             The information array, for attribute information, refer to documentation of
-     *                                                table tl_metamodel_attribute and documentation of the certain attribute
-     *                                                classes for information what values are understood.
+     * @param array $data                             The information array, for attribute information, refer to
+     *                                                documentation of table tl_metamodel_attribute and documentation
+     *                                                of the certain attribute classes for information what values are
+     *                                                understood.
      * @param Connection|null       $connection       The database connection.
      * @param TableManipulator|null $tableManipulator The table manipulator.
      * @param Adapter|null          $input            The input provider.
