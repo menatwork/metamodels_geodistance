252c23f6f17b2bd94f381c004fd4d30753740d9a
Mini Model <minimodel@metamodel.me>
2020-01-19 13:22:44 +0100

    Updated translations from transifex.

--- src/Resources/contao/languages/de/tl_metamodel_attribute.php
+++ src/Resources/contao/languages/de/tl_metamodel_attribute.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of MetaModels/attribute_geodistance.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2020 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -12,9 +12,9 @@
  * Translations are managed automatically using Transifex. To create a new translation
  * or to help to maintain an existing one, please register at transifex.com.
  *
- * Last-updated: 2019-03-26T19:48:22+01:00
+ * Last-updated: 2020-01-19T13:22:38+01:00
  *
- * @copyright 2012-2019 The MetaModels team.
+ * @copyright 2012-2020 The MetaModels team.
  * @license   https://github.com/MetaModels/attribute_geodistance/blob/master/LICENSE LGPL-3.0-or-later
  * @link      https://www.transifex.com/metamodels/public/
  * @link      https://www.transifex.com/signup/?join_project=metamodels
@@ -42,7 +42,7 @@ $GLOBALS['TL_LANG']['tl_metamodel_attribute']['get_geo']['1']                   
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['get_land']['0']                       = 'GET-Parameter für Land';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['get_land']['1']                       = 'Hier können Sie einen GET-Parameter für ein Land vorgeben.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['lookupservice']['0']                  = 'LookUp Services';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['lookupservice']['1']                  = 'Hier können Sie auswählen, welcher Dienst benutzt wird um Adressdaten aufzulösen.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['lookupservice']['1']                  = 'Hier können Sie auswählen, welcher Dienst benutzt wird, um Adressdaten aufzulösen.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['lookupservice']['api_token']['0']     = 'API Token';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['lookupservice']['api_token']['1']     = 'Hier können Sie ein API-Token angeben.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['parameter_legend']                    = 'Adressen-Parameter';
@@ -52,6 +52,6 @@ $GLOBALS['TL_LANG']['tl_metamodel_attribute']['perimetersearch']['open_street_ma
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['second_attr_id']['0']                 = 'Attribut - geografische Länge (Lng)';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['second_attr_id']['1']                 = 'Bitte wählen Sie das Attribut für den geografischen Längenwert aus.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['single_attr_id']['0']                 = 'Attribut';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['single_attr_id']['1']                 = 'Wählen Sie das Attribut aus in dem die Werte für geografische Länge und Breite gespeichert sind.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['single_attr_id']['1']                 = 'Wählen Sie das Attribut aus, in dem die Werte für geografische Länge und Breite gespeichert sind.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['geodistance']          = 'Geo-Entfernung';
 
